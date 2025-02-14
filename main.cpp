d3d95162437439e2c07e5806c451046f85df46dc
ilnarildarovuch <ilnarildarovuch@gmail.ru>
2025-02-15 02:09:26 +0500

    Translate comments

--- main.cpp
+++ main.cpp
@@ -4,63 +4,63 @@
 
 int main() {
     int n, public_key, private_key;
-    generateKeys(n, public_key, private_key); // Generate keys for signing
-    std::cout << "N for Publickey and Privatekey: " << n << std::endl;
-    std::cout << "Publickey: " << public_key << std::endl;
-    std::cout << "Privatekey: " << private_key << std::endl;
+    generateKeys(n, public_key, private_key); // Генерируем ключи для подписи
+    std::cout << "N для открытого и закрытого ключей: " << n << std::endl;
+    std::cout << "Открытый ключ: " << public_key << std::endl;
+    std::cout << "Закрытый ключ: " << private_key << std::endl;
 
     int userBalance = 0;
     std::vector<Transaction> transactions;
     std::vector<Transaction> transactions_all;
     if (userBalance >= 10) {
         transactions.emplace_back(to_string(public_key), 10, to_string(public_key));
-        transactions.back().signTransaction(private_key, n); // Sign the transaction
+        transactions.back().signTransaction(private_key, n); // Подписываем транзакцию
         transactions.emplace_back(to_string(public_key), 5, to_string(public_key));
-        transactions.back().signTransaction(private_key, n); // Sign the transaction
+        transactions.back().signTransaction(private_key, n); // Подписываем транзакцию
     }
     
-    // Create the genesis block
+    // Создаем блок генезис
     Block genesisBlock(0, "0", transactions, public_key);
     genesisBlock.displayBlock();
     transactions_all.insert(transactions_all.end(), genesisBlock.transactions.begin(), genesisBlock.transactions.end());
     userBalance = getBalance(to_string(public_key), transactions_all);
 
-    int blockCount = 1; // To keep track of the number of blocks created
+    int blockCount = 1; // счетчик созданных блоков
 
     while (true) {
-        // Create new transactions for the next block
+        // Создаем новые транзакции для следующего блока
         transactions.clear();
         if (userBalance >= 13) {
             transactions.emplace_back(to_string(public_key), 12, to_string(public_key));
-            transactions.back().signTransaction(private_key, n); // Sign the transaction
+            transactions.back().signTransaction(private_key, n); // Подписываем транзакцию
             transactions.emplace_back(to_string(public_key), 13, to_string(public_key));
-            transactions.back().signTransaction(private_key, n); // Sign the transaction
+            transactions.back().signTransaction(private_key, n); // Подписываем транзакцию
         }
-        // Create a new block
+        // Создаем новый блок
         Block newBlock(blockCount, genesisBlock.hash, transactions, public_key);
         newBlock.displayBlock();
         transactions_all.insert(transactions_all.end(), genesisBlock.transactions.begin(), genesisBlock.transactions.end());
 
-        // Verify transactions in the new block
+        // Проверяем транзакции в новом блоке
         for (const auto& tx : newBlock.transactions) {
             if (tx.publicKey == "0"){
-                std::cout << "Mined block coins: " << tx.howMany << ", to: " << tx.receiver_publicKey << std::endl;
+                std::cout << "Монеты, добытые в блоке: " << tx.howMany << ", на адрес: " << tx.receiver_publicKey << std::endl;
             }
             else if (tx.verifyTransaction(n)) {
-                std::cout << "Transaction verified: " << tx.publicKey << " -> " << tx.receiver_publicKey << " How much coins: " << tx.howMany << std::endl;
+                std::cout << "Транзакция подтверждена: " << tx.publicKey << " -> " << tx.receiver_publicKey << " Количество монет: " << tx.howMany << std::endl;
             } 
             else {
-                std::cout << "Transaction verification failed: " << tx.publicKey << " -> " << tx.receiver_publicKey << " How much coins: " << tx.howMany << std::endl;
+                std::cout << "Подтверждение транзакции неудалось: " << tx.publicKey << " -> " << tx.receiver_publicKey << " Количество монет: " << tx.howMany << std::endl;
             }
         }
         userBalance = getBalance(to_string(public_key), transactions_all);
-        std::cout << "User balance: " << userBalance << std::endl;
+        std::cout << "Баланс пользователя: " << userBalance << std::endl;
         std::cout << "------------------" << std::endl;
 
-        // Update the previous block to the new block
+        // Обновляем предыдущий блок до нового блока
         genesisBlock = newBlock;
 
-        // Increment the block count
+        // Увеличиваем счетчик блоков
         blockCount++;
     }
 
